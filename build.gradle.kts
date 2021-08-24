8e046352034677da810bc49f1865920c1344be9f
Rafael Cruz <rafael.cruz@wavy.global>
2021-08-24 16:07:51 -0300

    adding db

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.flywaydb:flyway-core")
+	runtimeOnly("org.postgresql:postgresql")
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	implementation("io.springfox:springfox-boot-starter:$swaggerVersion")
---
+++ src/main/resources/db.migration/V1__init_schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE public.city (
+    id        BIGSERIAL PRIMARY KEY,
+    city_name VARCHAR(255)
+);
